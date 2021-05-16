d08784ca5f881790fde96b7f48c2bffcf41a9df4
Raymond Tran <me@raytran.net>
2021-05-15 22:28:07 -0400

    Remove node modules from repo

--- node_modules/@roxi/routify/typings/lib/services/metaParser.d.ts
+++
@@ -1,8 +0,0 @@
-declare function _exports({ cache: useCache }?: {
-    cache: any;
-}): {
-    get: (file: any) => Promise<{}>;
-    hasMetaChanged: (file: any) => Promise<boolean>;
-    deleteFile: (file: any) => boolean;
-};
-export = _exports;
